deac07f3d82d981adecb7dfdfe3fc297a1699c12
Cristian Hernandez <93691947+cristianjhd92@users.noreply.github.com>
2025-03-08 08:34:11 +0000

    Agrega redireccionamiento al iniciar el app (cambio en dashboard.py) nueva funcion home

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,11 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 
 # Blueprint de Dashboard
 dash_bp = Blueprint('dashboard', __name__)
 
+@dash_bp.route('/')
+@login_required
+
+def home():
+    if current_user.rol == 'Administrador':
+        return redirect(url_for('dashboard.admin_dashboard'))
+    elif current_user.rol == 'Supervisor':
+        return redirect(url_for('dashboard.supervisor_dashboard'))
+    elif current_user.rol == 'Maestro de Obra':
+        return redirect(url_for('dashboard.maestro_dashboard'))
+    else:
+        return "Rol no reconocido", 403
+
 @dash_bp.route('/admin')
 @login_required
+
 def admin_dashboard():
     if current_user.rol != 'Administrador':
         return "Acceso no autorizado", 403
@@ -13,6 +27,7 @@ def admin_dashboard():
 
 @dash_bp.route('/supervisor')
 @login_required
+
 def supervisor_dashboard():
     if current_user.rol != 'Supervisor':
         return "Acceso no autorizado", 403
@@ -20,6 +35,7 @@ def supervisor_dashboard():
 
 @dash_bp.route('/maestro')
 @login_required
+
 def maestro_dashboard():
     if current_user.rol != 'Maestro de Obra':
         return "Acceso no autorizado", 403
